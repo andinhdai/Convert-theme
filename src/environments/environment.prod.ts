ce3cd474604bd1e5de6d46d25efc9048a34f9439
Dinh Dai <andinhdai@gmail.com>
2024-08-11 21:42:59 +0700

    fix5

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+
+export const environment = {
+    production: true,
+    // Các cấu hình khác cho môi trường sản xuất
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+2// src/environments/environment.ts
+
+export const environment = {
+    production: false,
+    // Các cấu hình khác cho môi trường phát triển
+  };
+  
